5e57adf8f7528e3a16ab349303cd51f36b82fdcb
Mike Errington <mike.errington@gmail.com>
2016-01-08 15:12:05 -0500

    Able to find all possible combinations of letters

--- lib/word.ex
+++ lib/word.ex
@@ -2,16 +2,15 @@ defmodule Wordbrainiac.Word do
   def find(board, length) do
     board
     |> build_moves
-    |> build_paths
+    |> build_paths(length)
+    |> build_words(board)
     # 1. build all "length" strings
     # 2. build a regex using
-
-    #Enum.map(Map.size(board), )
   end
 
   def build_moves(board) do
     Enum.reduce(board, Map.new, fn {rowNum, row}, map ->
-      Enum.reduce(row, map, fn {colNum, cell}, map ->
+      Enum.reduce(row, map, fn {colNum, _cell}, map ->
         case Map.has_key?(map, rowNum) do
           true ->
             Map.put(map, rowNum, Map.put(map[rowNum], colNum, find_neighbors(board, rowNum, colNum)))
@@ -33,38 +32,41 @@ defmodule Wordbrainiac.Word do
     end)
   end
 
-  defp valid_tile(board, row, col) do
-    Map.has_key?(board, row) and Map.has_key?(board[row], col)
+  defp valid_tile(board, row, col), do: Map.has_key?(board, row) and Map.has_key?(board[row], col)
+
+  def build_paths(moveMap, length) do
+    Enum.reduce(moveMap, [], fn {rowNum, row}, paths ->
+      Enum.reduce(row, paths, fn {colNum, _cell}, paths ->
+        [add_letter(moveMap, [{rowNum, colNum}], rowNum, colNum, length-1) | paths]
+      end)
+    end)
+    |> flatten_paths
   end
 
-  def build_paths(moveMap) do
-    #take the current "path" being built, copy it as many times as there are possible next moves (length(cell))
-    Enum.reduce(moveMap, [], fn {rowNum, row}, paths ->
-      Enum.reduce(row, paths, fn {colNum, cell}, paths ->
-        Enum.map(paths, fn path ->
+  defp add_letter(_moveMap, path, _rowNum, _colNum, length) when length == 0, do: path
 
-        end)
-      end)
+  defp add_letter(moveMap, path, rowNum, colNum, length) do
+    Enum.filter_map(moveMap[rowNum][colNum], &(!Enum.member?(path, &1)), fn nextCell = {nextCellRow, nextCellCol} ->
+      add_letter(moveMap, [nextCell | path], nextCellRow, nextCellCol, (length-1))
     end)
   end
 
-#  def build_word(board, path, length) when length == 0 do
-#    Enum.reverse(path)
-#    |> Enum.map_join(&([board[elem(&1, 0)][elem(&1,1)]]))
-#  end
-#  def build_word(board, path, length) do
-#    Enum.each(-1..1, fn(x) ->
-#      Enum.each(-1..1, fn(y) ->
-#        case {elem(hd(path), 0) + x, elem(hd(path), 1) + y} do
-#           {a, b} when a >= 0 and b >= 0-> ''
-#             if !Enum.member?(path, {x, y}) do
-#               build_word(board, [{a, b} | path], length-1)
-#             end
-#           _ -> ''
-#        end
-#      end )
-#    end )
-#  end
+  def flatten_paths(paths), do: do_flatten(paths, [])
+  def do_flatten([ head | tail ], tail2) when is_list(head) and is_list(hd(head)) do
+    do_flatten(head, do_flatten(tail, tail2))
+  end
+  def do_flatten([ head | tail ], tail2) do
+     [ head | do_flatten(tail, tail2) ]
+  end
+  def do_flatten([], tail) do
+    tail
+  end
 
+  def build_words(paths, board) do
+    Enum.map(paths, fn(path) ->
+      Enum.reverse(path)
+      |> Enum.map_join(&([board[elem(&1, 0)][elem(&1,1)]]))
+    end)
+  end
 
 end
--- test/word_test.exs
+++ test/word_test.exs
@@ -1,27 +1,6 @@
 defmodule WordTest do
   use ExUnit.Case
 
-#  test "find a list of words" do
-#    board = %{
-#      0 => %{ 0 => ?l, 1 => ?a },
-#      1 => %{ 0 => ?t, 1 => ?e }
-#    }
-#
-#    assert Enum.member?(Wordbrainiac.Word.find(board, 4), "late")
-#  end
-#
-#  test "building a complete word" do
-#    board = %{
-#      0 => %{ 0 => ?l, 1 => ?a },
-#      1 => %{ 0 => ?t, 1 => ?e }
-#    }
-#
-#    path = [{1,1}, {1, 0}, {0, 1}, {0,0}]
-#    #path = [[1, 1], [1, 0], [0, 1], [0, 0]]
-#
-#    assert Wordbrainiac.Word.build_word(board, path, 0) == "late"
-#  end
-
   test "find neighbours" do
     board = Wordbrainiac.Board.setup(2, 'late')
     neighbours = Wordbrainiac.Word.find_neighbors(board, 0, 0)
@@ -48,16 +27,29 @@ defmodule WordTest do
     assert length(moves[0][0]) == 3
   end
 
-#  test "finding a path of letters" do
-#    board = %{
-#      0 => %{ 0 => ?l, 1 => ?a },
-#      1 => %{ 0 => ?t, 1 => ?e }
-#    }
-#
-#
-#    path = [{0, 0}]
-#    #path = [[0, 0]]
-#
-#    assert length Wordbrainiac.Word.build_word(board, path, 4) == 6
-#  end
+  test "finding a path of letters" do
+    board = %{
+      0 => %{ 0 => ?l, 1 => ?a },
+      1 => %{ 0 => ?t, 1 => ?e }
+    }
+
+    moveMap = Wordbrainiac.Word.build_moves(board)
+
+    assert length(Wordbrainiac.Word.build_paths(moveMap, 4)) == 24
+  end
+
+  test "build a list of strings" do
+    board = %{
+      0 => %{ 0 => ?l, 1 => ?a },
+      1 => %{ 0 => ?t, 1 => ?e }
+    }
+
+    moveMap = Wordbrainiac.Word.build_moves(board)
+    paths = Wordbrainiac.Word.build_paths(moveMap, 4)
+
+    words = Wordbrainiac.Word.build_words(paths, board)
+
+    assert length(words) == 24
+    assert Enum.member?(words, "late")
+  end
 end
