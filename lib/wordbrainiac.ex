c149c9a66fdcc5310b49062aa1d7cd641bf767fb
Mike Errington <mike.errington@gmail.com>
2016-01-12 14:08:31 -0500

    Changing to better (bigger) dictionary

--- lib/wordbrainiac.ex
+++ lib/wordbrainiac.ex
@@ -1,13 +1,13 @@
 defmodule Wordbrainiac do
   alias Wordbrainiac.{Board, Word, Dictionary}
 
-  def process(letters, word_lengths, dictionary_file \\ "wordlist.txt") do
+  def process(letters, word_lengths, dictionary_file \\ "words.txt") do
     dictionary = Dictionary.get_dict_trie(dictionary_file)
     Board.setup(letters)
     |> find_next_word(word_lengths, dictionary, [])
   end
 
-  def find_next_word(board, word_lengths, dictionary, words) when length(word_lengths) == 0 do
+  def find_next_word(_board, word_lengths, _dictionary, words) when length(word_lengths) == 0 do
     words
   end
 
@@ -15,10 +15,12 @@ defmodule Wordbrainiac do
     board
     |> Word.find(hd(word_lengths))
     |> Dictionary.check_words(dictionary)
-    |> Enum.reduce(solutions, fn (solution = %{:path => path, :word => word}, solutions) ->
-    #|> Enum.map(fn (solution = %{:path => path, :word => word}) ->
-      Board.remove_path(board, path)
-      |> find_next_word(tl(word_lengths), dictionary, solutions ++ [solution])
+    #|> Enum.reduce(solutions, fn (solution = %{:path => path, :word => _word}, solutions) ->
+    |> Enum.map(fn (solution = %{:path => path, :word => word}) ->
+      List.flatten(
+        Board.remove_path(board, path)
+        |> find_next_word(tl(word_lengths), dictionary, solutions ++ [solution])
+      )
     end)
   end
 end
